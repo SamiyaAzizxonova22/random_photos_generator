5067b09d330f4d9803d6ddaedc697dd91255588b
samiyaazizxonova <samiyaazizxonova2000@gmail.com>
2023-01-17 10:12:54 +0500

    website fixed

--- js/main.js
+++ js/main.js
@@ -2,19 +2,22 @@ const rasmlar = document.querySelector(".photos__list");
 const form = document.querySelector(".form");
 const error = document.querySelector('.value_error')
 
-
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	const api = `https://api.unsplash.com/search/photos/?page=1&query=${form.input.value}&client_id=PYYovcuRItu-VvSKDkIOgkLY8QDIQ5X8CXQijeIUxgU`;
-	getTodos(api)
-		.then((data) => {
-			// getTodos(data);
-			// console.log("ishladi");
-			showPhotos(data);
-		})
-		.catch((err) => {
-			console.log(err, "xatolik mavjud");
-		});
+	if (form.input.value.trim() == ''){
+		error.textContent = 'Iltimos yoqtirgan rasmingiz nomini kiriting !!!'
+		console.log('input bosh qoldi')
+	}else{
+
+		const api = `https://api.unsplash.com/search/photos/?page=1&query=${form.input.value}&client_id=PYYovcuRItu-VvSKDkIOgkLY8QDIQ5X8CXQijeIUxgU`;
+		getTodos(api)
+			.then((data) => {
+				showPhotos(data)
+			})
+			.catch((err) => {
+				console.log(err, "xatolik mavjud");
+			});
+	}
 });
 
 const getTodos = (resurse) => {
@@ -27,31 +30,31 @@ const getTodos = (resurse) => {
 				resolve(data);
         error.textContent = ''
         rasmlar.style.display = 'grid'
+				console.log(data)
+
+				if(data.total == 0){
+					error.textContent = 'Iltimos yoqtirgan rasmingiz nomini kiriting !!!'
+					console.log('error ishladi')
+				}
 
 
-			} else if(request.readyState === 4 && form.input.value.length === 0){
-        error.textContent = "Iltimos yoqtirgan rasmingiz nomini kiriting !!!"
-        rasmlar.style.display = 'none'
 
-			}
-			else if (request.readyState === 4) {
+			}else	if(request.readyState === 4){
 				reject(
-					error.textContent = "Iltimos yoqtirgan rasmingiz nomini kiriting !!!"
-				);
-        rasmlar.style.display = 'none'
-			}
+					error.textContent = "Iltimos rasmingiz nomini tekshiring !!!"
+					);
+				console.log('end ishladi')
+				}
 		});
-
 		request.open("GET", resurse);
 		request.send();
 	});
 };
+
 getTodos(
 	`https://api.unsplash.com/photos/?client_id=PYYovcuRItu-VvSKDkIOgkLY8QDIQ5X8CXQijeIUxgU`
 )
 	.then((data) => {
-		// getTodos(data);
-		// console.log("ishladi");
 		showPhotos(data);
 	})
 	.catch((err) => {
@@ -62,7 +65,8 @@ getTodos(
 
 function showPhotos(foto) {
 	rasmlar.innerHTML = "";
-	(foto.results ? foto.results : foto).forEach((item) => {
+	const photos = foto.results ? foto.results : foto;
+	photos.forEach((item) => {
 		const { likes, user, urls } = item;
 
 		rasmlar.innerHTML += `
